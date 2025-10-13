3b34cceed513b37509b311ec3699111b4e9bbc7b
theotime2005 <theotime.berthod@epitech.digital>
2025-10-13 15:33:10 +0200

    add constants and errors

---
+++ api/src/identities-access-management/errors.js
@@ -0,0 +1,9 @@
+const ERRORS = {
+  TOKEN: {
+    EXPIRED_TOKEN: "Token has expired",
+    INVALID_TOKEN: "Invalid token",
+    VERIFICATION_FAILED: "Token verification failed",
+  },
+};
+
+export default ERRORS;
---
+++ api/src/shared/constants.js
@@ -0,0 +1,7 @@
+const USER_TYPE = {
+  ADMIN: "admin",
+  MANAGER: "manager",
+  EMPLOYER: "employer",
+};
+
+export { USER_TYPE };
