6b25a29d24d7e68686ee0b33a09be6061d625b97
theotime2005 <theotime.berthod@epitech.digital>
2025-10-20 16:34:54 +0200

    add some constants

--- admin/src/constants.js
+++ admin/src/constants.js
@@ -1,6 +1,12 @@
+const USER_TYPE = {
+  ADMIN: "admin",
+  MANAGER: "manager",
+  EMPLOYER: "employer",
+};
+
 const ERRORS = {
   INVALID_CREDENTIALS: "Invalid credentials",
   API_ERROR: "Server error, please try again later",
 };
 
-export { ERRORS };
+export { ERRORS, USER_TYPE };
